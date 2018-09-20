43efc00d2f8c7744b4c3f5a754bc5772759fca7a
pkruthi <kruthi.purohit@mcg.ai>
2018-09-20 16:22:08 +0530

    Second commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+pandita
+=======
+
+XAI project
---
+++ pandita/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    XAI project
+
+    :copyright: (c) 2018 by Kruthi.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'pandita'
+__version__ = '0.0.1'
+__author__ = 'Kruthi'
